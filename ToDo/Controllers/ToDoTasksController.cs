d60d29571de7253843e8ea461b17223b1e10f389
Oscar <Oscarponteuk@gmail.com>
2020-08-04 16:09:10 +0100

    Create initial models and views

---
+++ ToDo/Controllers/ToDoTasksController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    public class ToDoTasksController : Controller
+    {
+        // GET: Tasks
+        public ViewResult Index()
+        {
+            var tasks = GetTasks();
+
+            return View(tasks);
+        }
+        public ActionResult Details(int id)
+        {
+            var tasks = GetTasks().SingleOrDefault(t => t.Id == id);
+
+            if (tasks == null)
+                return HttpNotFound();
+
+            return View(tasks);
+        }
+        private IEnumerable<ToDoTask> GetTasks()
+        {
+            return new List<ToDoTask>
+            {
+                new ToDoTask {Id = 1, Name = "Clean The Bathroom"},
+                new ToDoTask {Id = 2, Name = "Clean The Kitchen"},
+                new ToDoTask {Id = 3, Name = "Clean The Bedroom"}
+            };
+
+        }
+
+    }
+}
---
+++ ToDo/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    public class UsersController : Controller
+    {
+     
+     public ViewResult Index()
+    {
+        var users = GetUsers();
+
+        return View(users);
+    }
+
+    public ActionResult Details(int id)
+    {
+        var users = GetUsers().SingleOrDefault(u => u.Id == id);
+
+        if (users == null)
+            return HttpNotFound();
+
+        return View(users);
+    }
+
+    private IEnumerable<User> GetUsers()
+    {
+            return new List<User>
+                {
+                    new User { Id = 1, Name = "Oscar" },
+                    new User { Id = 2, Name = "Rossy" }
+                };
+        }
+    }
+} 
